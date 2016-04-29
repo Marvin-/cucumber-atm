dc3a6a34893e3aca278f430c4cf9277285d603e4
Marvin M <marvin23m@gmail.com>
2016-04-29 18:21:51 +0100

    Complete last step with a CashSlot class. Refactor code to include methods in world module. Make sure cucumber tests pass

--- features/step_definitions/step.rb
+++ features/step_definitions/step.rb
@@ -9,34 +9,56 @@ class Account
 end
 
 class Teller
+  def initialize(cash_slot)
+    @cash_slot = cash_slot
+  end
+
   def withdraw_from(account, amount) 
+    @cash_slot.dispense(amount)
   end
 end
 
-module KnowsMyAccount 
+class CashSlot 
+  def contents
+    @contents or raise("I'm empty!") 
+  end
+
+  def dispense(amount)
+    @contents = amount
+  end
+end
+
+module KnowsTheDomain 
   def my_account
     @my_account ||= Account.new
   end 
-end
-World(KnowsMyAccount)
 
-CAPTURE_A_NUMBER = Transform /^\$(\d+)$/ do |digits| 
+  def cash_slot
+    @cash_slot ||= CashSlot.new
+  end
+
+  def teller
+    @teller ||= Teller.new(cash_slot)
+  end
+end
+World(KnowsTheDomain)
+
+CAPTURE_CASH_AMOUNT  = Transform /^\$(\d+)$/ do |digits| 
   digits.to_i
 end
 
-Given(/^I have deposited (#{CAPTURE_A_NUMBER}) in my account$/) do |amount|
+Given(/^I have deposited (#{CAPTURE_CASH_AMOUNT }) in my account$/) do |amount|
   my_account = Account.new
   my_account.deposit(amount)
   expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
-When(/^I withdraw (#{CAPTURE_A_NUMBER})$/) do |amount|
-teller = Teller.new
-teller.withdraw_from(my_account, amount)
+When(/^I withdraw (#{CAPTURE_CASH_AMOUNT })$/) do |amount|
+  teller.withdraw_from(my_account, amount)
 end
 
-Then(/^\$(\d+) should be dispensed$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/) do |amount|
+  expect(cash_slot.contents).to eq(amount)
 end
 
 
