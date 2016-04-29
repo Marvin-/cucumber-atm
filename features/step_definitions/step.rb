3544a7aa0bb2620ac31a28ae417cce13751171f3
Marvin M <marvin23m@gmail.com>
2016-04-29 17:11:46 +0100

    Complete second cucumber step with new Teller class and a module to store state in cucumber 'World'

--- features/step_definitions/step.rb
+++ features/step_definitions/step.rb
@@ -8,20 +8,37 @@ class Account
   end
 end
 
-CAPTURE_A_NUMBER = Transform(/^\d+$/) do |number| 
-  number.to_i
+class Teller
+  def withdraw_from(account, amount) 
+  end
 end
 
-Given(/^I have deposited \$(#{CAPTURE_A_NUMBER}) in my account$/) do |amount|
+module KnowsMyAccount 
+  def my_account
+    @my_account ||= Account.new
+  end 
+end
+World(KnowsMyAccount)
+
+CAPTURE_A_NUMBER = Transform /^\$(\d+)$/ do |digits| 
+  digits.to_i
+end
+
+Given(/^I have deposited (#{CAPTURE_A_NUMBER}) in my account$/) do |amount|
   my_account = Account.new
   my_account.deposit(amount)
   expect(my_account.balance).to eq(amount), "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
-When(/^I request \$(\d+)$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I withdraw (#{CAPTURE_A_NUMBER})$/) do |amount|
+teller = Teller.new
+teller.withdraw_from(my_account, amount)
 end
 
 Then(/^\$(\d+) should be dispensed$/) do |arg1|
   pending # Write code here that turns the phrase above into concrete actions
 end
+
+
+
+
